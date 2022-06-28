0c7521f60180cae7e65ebc93ba8de49bee92ee32
Mike Jackson <hi@mikejackson.co.uk>
2022-06-28 19:23:16 +0100

    UI: Add various game regions
    Add regions for the board, card detail, deck, discard, resource pool,
    life total and phase indicator based on a fixed 1024x768 resolution.

--- pyshandalar/__main__.py
+++ pyshandalar/__main__.py
@@ -1,10 +1,13 @@
 # A simple demo game
 import pygame
 
+card_detail_rect = (0, 192, 260, 384)
+
 if __name__ == "__main__":
     pygame.init()
-
-    screen = pygame.display.set_mode([500, 500])
+    
+    pygame.display.set_caption("Magik: the Happening")
+    screen = pygame.display.set_mode([1024, 768])
 
     running = True
     while running:
@@ -13,7 +16,49 @@ if __name__ == "__main__":
                 running = False
 
         screen.fill((255, 255, 255))
-        pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+        # card detail
+        pygame.draw.rect(screen, (120, 74, 48), (0, 192, 260, 384))
+
+        # phase panel
+        pygame.draw.rect(screen, (150, 150, 150), (260, 0, 40, 768))
+
+        # player 1 board
+        pygame.draw.rect(screen, (0, 255, 0), (300, 0, 724, 384))
+
+        # player 1 deck
+        pygame.draw.rect(screen, (255, 0, 0), (0, 59, 90, 133))
+
+        # player 1 graveyard
+        pygame.draw.rect(screen, (255, 255, 0), (90, 59, 90, 133))
+
+        # player 1 mana pool
+        pygame.draw.rect(screen, (0, 0, 255), (180, 0, 80, 192))
+
+        # player 1 life total
+        pygame.draw.rect(screen, (150, 150, 150), (0, 0, 180, 59))
+
+        # player 2 board
+        pygame.draw.rect(screen, (0, 0, 255), (300, 384, 724, 768))
+
+        # player 2 deck
+        pygame.draw.rect(screen, (255, 0, 0), (0, 635, 90, 133))
+
+        # player 2 graveyard
+        pygame.draw.rect(screen, (255, 255, 0), (90, 635, 90, 133))
+
+        # player 2 mana pool
+        pygame.draw.rect(screen, (0, 0, 255), (180, 576, 80, 192))
+
+        # player 2 life total
+        pygame.draw.rect(screen, (150, 150, 150), (0, 576, 180, 59))
+
+        # some text
+        default_font = pygame.font.SysFont("Verdana", 24)
+        text = default_font.render("Magik: the Happening", True, (255, 255, 255))
+
+        screen.blit(text, (500, 384))
+
         pygame.display.flip()
 
     pygame.quit()
